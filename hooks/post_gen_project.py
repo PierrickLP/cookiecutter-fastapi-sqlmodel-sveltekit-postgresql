cf683a24af44193bc06c6e0134a3a8992d11a686
PierrickLP <pierrickleplenier@gmail.com>
2023-04-07 18:04:22 +0200

    fix cookiecutter

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,4 +5,4 @@ path: Path
 for path in Path(".").glob("**/*.sh"):
     data = path.read_bytes()
     lf_data = data.replace(b"\r\n", b"\n")
-    path.write_bytes(lf_data)from pathlib import Path
+    path.write_bytes(lf_data)
